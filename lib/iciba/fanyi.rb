696aaddd2a1eed38baca7181040c93dfbf3f7c40
Scott Ballantyne <ussballantyne@gmail.com>
2012-09-10 09:29:24 +0800

    added optional pinyin

--- lib/iciba/fanyi.rb
+++ lib/iciba/fanyi.rb
@@ -1,18 +1,22 @@
 # coding: utf-8
+require 'string_to_pinyin'
 module Iciba
   class Fanyi
     attr_accessor :response, :html, :dir, :result
 
     
 
-    def initialize(words, dir='auto', bin=false, extended=false)
+    def initialize(words, dir='auto', bin=false, extended=false, pinyin=false)
       puts '------------------------------------------------------------------------------------' if bin
       # puts if bingit 
       puts wrap_text(words, 40) if words.contains_cjk? && bin
       puts wrap_text(words) unless words.contains_cjk? && bin
+      puts '------------------------------------------------------------------------------------' if words.contains_cjk? && bin && pinyin
+      puts words.to_pinyin if words.contains_cjk? && bin && pinyin
       puts '------------------------------------------------------------------------------------' if bin
       download_and_parse(words, dir, extended)
       puts wrap_text(self.result) if bin
+      puts self.result.to_pinyin if self.result.contains_cjk? && bin
       puts '------------------------------------------------------------------------------------' if bin
       return self.result
     end
@@ -24,6 +28,14 @@ module Iciba
     
     def self.check_char(str)
       str.contains_cjk?
+    end
+    
+    def print_pinyin(words)
+      puts words.string_to_pinyin
+    end
+    
+    def self.print_pinyin(words)
+      puts words.string_to_pinyin
     end
   
   
